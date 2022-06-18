f5b949b2d5d805db25ba4881cc3d3636b251688f
TrinaNix4 <98623374+TrinaNix4@users.noreply.github.com>
2022-06-18 13:21:29 -0600

    changed welcome message on home page

--- client/src/components/shared/Home.js
+++ client/src/components/shared/Home.js
@@ -7,12 +7,12 @@ const Home = () => {
 
   return (
     <div className='page'>
-      <h1>Home</h1>
+      <h1>Welcome to Game On!</h1>
       <hr></hr>
-      <p>Welcome to the absolute best place to find the RIGHT games at the RIGHT price! You won't be disappointed with our selection! Get ready for your next adventure and GAME ON!</p>
+      <p>You've found the best place to find the RIGHT games at the RIGHT price! You won't be disappointed with our selection! Get ready for your next adventure and GAME ON!</p>
       <hr></hr>
       <p style={{ display: 'flex', justifyContent: 'space-around' }}>
-      <Link to='/products' className='home_link'>Search Games by Seller</Link> | <Link to='/categories' className='home_link'>Search Games by Category</Link> | <Link to='find_products' className='home_link'>Find Previously Sold Games</Link>
+      <Link to='/products' className='home_link'>Search Games by Seller</Link> | <Link to='/categories' className='home_link'>Search Games by Console</Link> | <Link to='find_products' className='home_link'>Find Previously Sold Games</Link>
       </p>
     </div>
   );
